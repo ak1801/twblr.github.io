980de526048970ebae0877b21fb8eca580024ca7
Akshit Mahajan -X (aksmahaj - GOLDSTONE TECHNOLOGIES LIMITED at Cisco) <aksmahaj@cisco.com>
2016-07-22 15:59:51 +0530

    updated switch_structure code

--- control_structures/if_structure.go
+++ control_structures/if_structure.go
@@ -1,5 +1,8 @@
 package control_structures
-import "fmt"
+import (
+    "strconv"
+    "fmt"
+)
 func fizzBuzz(i int32) string {
 	var result = i;
 	
@@ -8,9 +11,9 @@ func fizzBuzz(i int32) string {
 	}else if i%5 == 0 {
 		result = Buzz 
 	}else if (i%3 == 0 && i%5 == 0) {
-		result = fizzBuzz 
+		result = FizzBuzz 
 	} else {
-		result = fmt.Sprintf("%d", i)
+		result = strconv.Itoa(i)//fmt.Sprintf("%d", i)
 	}
 
 	return result
