6d0cd991fb91e2bb486b45a472da7956419aabfd
Akshit Mahajan -X (aksmahaj - GOLDSTONE TECHNOLOGIES LIMITED at Cisco) <aksmahaj@cisco.com>
2016-07-22 16:10:41 +0530

    updated for_structure code

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,14 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	return 0
+	count := 0
+	for(n != 1) {
+		if n%2 == 0 {
+			n = n/2
+		} else {
+			n = (3*n) + 1
+		}
+		count++
+	}
+	return count
 }
