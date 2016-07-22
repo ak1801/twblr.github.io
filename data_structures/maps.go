5dedc48bb8219f0d638cf814a27764ae8219570b
Akshit Mahajan -X (aksmahaj - GOLDSTONE TECHNOLOGIES LIMITED at Cisco) <aksmahaj@cisco.com>
2016-07-22 21:25:45 +0530

    added code for maps.go

--- data_structures/maps.go
+++ data_structures/maps.go
@@ -1,9 +1,24 @@
 package data_structures
 
 func findPeopleWithCommonInterest(data map[string][]string, interest string) []string {
-	return nil
+	
+	result := []string{}	//declare slice
+
+	for idx,val := range data {
+		if contains(val, interest) {
+			result = append(result, idx)
+		}
+	}
+	return result
 }
 
 func contains(src []string, elem string) bool {
-	return false
+	
+	var res bool = false
+	for _,value := range src {
+		if value==elem {
+			res = true
+		} 
+	}
+	return res
 }
