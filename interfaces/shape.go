101affd6db527fdd182d40148cbfcc73db600fa7
Akshit Mahajan -X (aksmahaj - GOLDSTONE TECHNOLOGIES LIMITED at Cisco) <aksmahaj@cisco.com>
2016-07-23 11:04:44 +0530

    Adding shape.go

--- interfaces/shape.go
+++ interfaces/shape.go
@@ -1,5 +1,31 @@
 package interfaces
 
+type Square struct {
+	side int
+}
+
+type Rectangle struct {
+	length int
+	breadth int
+}
+
+type Hybrid struct{
+	s1 Shape
+	s2 Shape
+}
+
 type Shape interface {
 	Area() int
+}
+
+func (sq Square) Area() int{
+	return sq.side*sq.side
+}
+
+func (rect Rectangle) Area() int{
+	return rect.length*rect.breadth
+}
+
+func (hybrid Hybrid) Area() int{
+	return hybrid.s1.Area() + hybrid.s2.Area()
 }
